456ba01b6aaaef384bf680843a121aef30cbebba
ule270 <u.le270@gmail.com>
2023-03-03 19:28:34 -0500

    I have added some code

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,10 +1,37 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        char operator;
+        double answer = 0.0;
+        Scanner scanner;
+    public void run() {
+            System.out.println("Enter a number: ");
+            num1 = scanner.nextInt();
 
+            System.out.print("Enter a second number: ");
+            num2 = scanner.nextInt();
 
+            System.out.println("Enter an operator: ");
+            operator = scanner.next().charAt(0);
 
-    public void run(){
-
+            switch (operator) {
+                case '+':
+                    System.out.println(num1 + num2);
+                    break;
+                case '-':
+                    System.out.println(num1 - num2);
+                    break;
+                case '*':
+                    System.out.println(num1 * num2);
+                    break;
+                case '/':
+                    System.out.println(num1 / num2);
+                    break;
+            }
+        }
     }
-}
