7a719b23ce6936561b3d790f03a81deade005673
Timothy <timothy@Zipcoders-MacBook-Pro-3.local>
2023-03-05 13:41:33 -0500

    new changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+
 // 1. Make while loop to ensure the application runs 24/7 until the user quits
 // 2. Make an instruction for if the user enters "quit", Break the while loop
 // 3. Every iteration of the while loop will ask the user for the scanner input.
@@ -16,12 +17,10 @@ public class MainApplication {
     // For instance : Add will expect a secondary value, Prompt the user to enter second value and press "=" when done
     // Every operator will return the result
 // 7. At the end of the while loop, the display will be shown to the user.
-
-    }
-
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double display = 0;
+        String operator = "";
         ScientificFeatures scientificFeatures = new ScientificFeatures();
         Calculator calculator = new Calculator();
 
@@ -31,29 +30,35 @@ public class MainApplication {
             System.out.println(display + "");
             // 4. Check the user input, see if they entered a number or an operator
             String userInput = sc.nextLine();
-            this.isNumeric(userInput);
             // 2. Make an instruction for if the user enters "quit", Break the while loop
             if (userInput == "quit") {
                 System.out.println("Calculator app will now quit.");
                 break;
             } else {
                 //if the user enters a double then make display = new double value
-                   if (userInput == a number){
-                       display = userInput;
-                       //if user enters another number into the display field, then concat/append both doubles into one
-                       if (userInput a 2nd number){
-                           display = this.concat(userInput, userInput2);
+                if (isNumeric(userInput)) {
+                    if (display == 0) {
+                        display = Double.parseDouble(userInput);
+
+                        //    display = Double(userInput);
+                    } else {
+                        //concat two int into one store into display
+                        display += Double.parseDouble(userInput);
                     }
                 }
+                //if user enters another number into the display field, then concat/append both doubles into one
+
+
                 // 5. Have an if statement that checks operator, maybe the if statment will be
                 // really long and check for every single operator
                 // Such as : Add/Divide/ Cos/ Log.. etc
-                   else if (userInput.equals(operator)){
+                else if (isNumeric(userInput) == false) {
                     scientificFeatures.run(userInput);
-                } else if (userInput.equals(operator)) {
+                } else if (isNumeric(userInput) == false) {
                     calculator.run(userInput);
                 }
             }
+
             // 6. Every operator will do something special with it's own logic.
             // For instance : Add will expect a secondary value, Prompt the user to enter in
             // a second value and press "=" when done
@@ -63,10 +68,11 @@ public class MainApplication {
             // (this is the last line System.out.println(display))
             System.out.println(display);
         }
+        }
 
-    }
 
-    public static boolean isNumeric(String userInput) {
+
+    public static boolean isNumeric(String userInput) { //change method name to paruse userinput
         try {
             Double.parseDouble(userInput);
             return true;
@@ -74,7 +80,7 @@ public class MainApplication {
             return false;
         }
     }
-    public int concat(int a, int b)
+    public static int concat(int a, int b)
     {
 
         // Convert both the integers to string
