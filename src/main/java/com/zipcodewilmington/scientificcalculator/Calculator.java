6719c82b677b2c244d38c7cdaf0b01be3ff3cb46
ule270 <u.le270@gmail.com>
2023-03-05 10:51:17 -0500

    Added ^, ~, 1/x, -1

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -3,37 +3,53 @@ package com.zipcodewilmington.scientificcalculator;
 import java.util.Scanner;
 
 public class Calculator {
-        int num1 = 0;
-        int num2 = 0;
-        int tempNum = 0;
-        char operator;
-        double answer = 0.0;
-        Scanner scanner = new Scanner(System.in);
-
+    double num1 = 0;
+    double num2 = 0;
+    char operator;
+    double result = 0.0;
+    Scanner scanner = new Scanner(System.in);
+    static String menu = "\nChoose an operation:\n+ Add\n- Subtract\n* Multiply\n/ Divide\n^ Exponent\n~ Square Root\n1/x Inverse\n-1 switchSign\n Exit";
 
     public void run(String userInput) {
-            System.out.println("Enter a number: ");
-            num1 = scanner.nextInt();
+        System.out.println(menu);
+        operator = scanner.next().charAt(0);
 
-            System.out.print("Enter a second number: ");
-            num2 = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        num1 = scanner.nextInt();
 
-            System.out.println("Enter an operator: ");
-            operator = scanner.next().charAt(0);
+        System.out.print("Enter a second number: ");
+        num2 = scanner.nextInt();
 
-            switch (operator) {
-                case '+':
-                    System.out.println(num1 + num2);
-                    break;
-                case '-':
-                    System.out.println(num1 - num2);
-                    break;
-                case '*':
-                    System.out.println(num1 * num2);
-                    break;
-                case '/':
-                    System.out.println(num1 / num2);
-                    break;
-            }
+        switch (operator) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            case '^':
+                System.out.println(Math.pow(num1,num2));
+                break;
+            case '~':
+                System.out.println(Math.sqrt(num1));
+                break;
+            case '1/x':
+                System.out.println(1/num1);
+                break;
+            case -1:
+                System.out.println(num1 *-1);
+                break;
+            default:
+                System.out.println("Error");
+                break;
         }
     }
+}
+
+
