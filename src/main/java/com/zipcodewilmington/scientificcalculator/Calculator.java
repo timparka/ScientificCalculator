2b23b9c877192f7bc3ef3bc083a4bc4df4ae1005
ule270 <u.le270@gmail.com>
2023-03-05 11:16:52 -0500

    Exit package added.

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
     double num1 = 0;
     double num2 = 0;
     char operator;
-    double result = 0.0;
     Scanner scanner = new Scanner(System.in);
     static String menu = "\nChoose an operation:\n+ Add\n- Subtract\n* Multiply\n/ Divide\n^ Exponent\n~ Square Root\n1/x Inverse\n-1 switchSign\n Exit";
 
@@ -31,7 +30,11 @@ public class Calculator {
                 System.out.println(num1 * num2);
                 break;
             case '/':
-                System.out.println(num1 / num2);
+                if (num2 !=0){
+                    System.out.println(num1 / num2);
+                } else {
+                    System.out.println("Cannot divide by "0);
+                };
                 break;
             case '^':
                 System.out.println(Math.pow(num1,num2));
@@ -39,14 +42,19 @@ public class Calculator {
             case '~':
                 System.out.println(Math.sqrt(num1));
                 break;
-            case '1/x':
+            case "1/x":
                 System.out.println(1/num1);
                 break;
             case -1:
                 System.out.println(num1 *-1);
                 break;
+            case "Exit":
+                System.out.println("You have exited calculator");
+                System.exit(0);
+                break;
             default:
                 System.out.println("Error");
+                System.exit(0);
                 break;
         }
     }
